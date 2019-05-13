5667a7960fdd94b2807a1c10d274be8e06992073
super-eben <eben@superquery.io>
2019-05-13 13:36:05 +0200

    New release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'superQuery',         # How you named your package folder (MyLib)
   packages = ['superQuery'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'The Python interface to superQuery',   # Give a short description about your library
   author = 'Eben du Toit',                   # Type in your name
   author_email = 'eben@superquery.io',      # Type in your E-Mail
   url = 'https://github.com/superquery/superPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/superquery/superPy/archive/v0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/superquery/superPy/archive/v0.3.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pymysql'
