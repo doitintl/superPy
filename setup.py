398f941d05b22c38d1feb5123abb2afef15358cb
super-eben <eben@superquery.io>
2019-05-13 13:16:07 +0200

    Updated download_url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Eben du Toit',                   # Type in your name
   author_email = 'eben@superquery.io',      # Type in your E-Mail
   url = 'https://github.com/superquery/superPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/superquery/superPy/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pymysql'
