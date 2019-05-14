02e99a6da259158f562bc280e3717e66a59df5d3
super-eben <eben@superquery.io>
2019-05-14 12:56:31 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'superQuery',         
   packages = ['superQuery'],   
-  version = '0.7',     
+  version = '0.8',     
   license='MIT',       
   description = 'The Python interface to superQuery',  
   author = 'Eben du Toit',                 
   author_email = 'eben@superquery.io',     
   url = 'https://github.com/superquery/superPy',  
-  download_url = 'https://github.com/superquery/superPy/archive/v0.7.tar.gz',    
+  download_url = 'https://github.com/superquery/superPy/archive/v0.8.tar.gz',    
   keywords = ['DATA', 'SUPERQUERY', 'BIGQUERY'],
   install_requires=[            
           'pymysql'
