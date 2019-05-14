6743a160690bd4bf4f96b0b1b2be283c9e4cc085
super-eben <eben@superquery.io>
2019-05-14 21:24:25 +0200

    Result class

--- examples/start_here.py
+++ examples/start_here.py
@@ -5,25 +5,28 @@ dryrun = False
 
 sq = SuperQuery()
 mydata = sq.get_data(
-    "SELECT field FROM `projectId.datasetId.tableID` WHERE _PARTITIONTIME = \"20xx-xx-xx\"", 
-    get_stats=True, 
-    dry_run=dryrun, 
-    username="xxxxxxxxx", 
-    password="xxxxxxxxx")
+    "SELECT field1, field2 FROM `projectId.datasetId.tableId` WHERE _PARTITIONTIME = \"2019-05-04\" ORDER BY field1 ASC", 
+    username="xxxxxxxxxx", 
+    password="xxxxxxxxxx", 
+    start=1, 
+    stop=10)
 
-print ("---------RESULTS---------")
+print ("---------STATS---------")
 if (not dryrun):
-    print("Data rows:", mydata.stats["totalRows"])
+    print("Data rows:", mydata.totalRows)
     print("Workflow:", "Query")
-    print("Cost:", mydata.stats["superQueryTotalCost"])
-    print("Savings %:", mydata.stats["saving"])
-    print("Was cache used?:", mydata.stats["cacheUsed"] if "cacheUsed" in mydata.stats else False)
-    print("DryRun flag: ", mydata.stats["superParams"]["isDryRun"])
+    print("Cost:", mydata.superQueryTotalCost)
+    print("Savings %:", mydata.saving)
+    print("Was cache used?:", mydata.cacheUsed if hasattr(mydata, "cacheUsed") else False)
+    print("DryRun flag: ", mydata.superParams["isDryRun"])
 else:
     print("Workflow:", "DryRun")
-    print("Potential BQ bytes scanned: ", mydata.stats["bigQueryTotalBytesProcessed"])
-    print("Potential Data rows:", mydata.stats["totalRows"])
-    print("DryRun flag: ", mydata.stats["superParams"]["isDryRun"])
+    print("Potential BQ bytes scanned: ", mydata.bigQueryTotalBytesProcessed)
+    print("Potential Data rows:", mydata.totalRows)
+    print("DryRun flag: ", mydata.superParams["isDryRun"])
+
+print ("---------10 ROWS OF DATA---------")
+for row in mydata:
+    print("Row :", row)
 
 del sq
-
