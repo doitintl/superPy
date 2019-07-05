bce86b6dbc50e064e2f126745b734c2c2a0e3a3b
super-eben <eben@superquery.io>
2019-07-05 20:50:40 +0200

    Base example update

--- examples/start.py
+++ examples/start.py
@@ -1,25 +1,27 @@
 from superQuery import superQuery
 
 client = superQuery.Client()
+# Select a project, otherwise the default project is selected
+# client.set_project_id("yourproject") 
 
-dryrun = False
+QUERY = """SELECT name FROM `bigquery-public-data.usa_names.usa_1910_current` LIMIT 10"""
 
-QUERY = """SELECT name FROM `bigquery-public-data.usa_names.usa_1910_current` LIMIT 50"""
-
-query_job = client.query(QUERY, username="xxxxxx", password="xxxxxx", project_id=None) 
+query_job = client.query(QUERY) 
 
 rows = query_job.result()
 
+print ("---------DATA---------")
 for row in rows:
     print(row.name)
 
 print ("---------STATISTICS---------")
-if (not dryrun):
+if (not query_job.superParams["isDryRun"]):
     print("Data rows:", query_job.totalRows)
     print("Workflow:", "Query")
-    print("Cost:", query_job.superQueryTotalCost)
+    print("Cost: $ %.2f" % round(query_job.superQueryTotalCost, 2))
     print("Savings %:", query_job.saving)
     print("Was cache used?:", query_job.cacheUsed if hasattr(query_job, "cacheUsed") else False)
+    print("Cache type:", query_job.cacheType if hasattr(query_job, "cacheUsed") else "None")
     print("DryRun flag: ", query_job.superParams["isDryRun"])
 else:
     print("Workflow:", "DryRun")
