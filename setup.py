8e640f73306485cb6084890198c4682b8f639735
Eben du Toit <eben@superquery.io>
2019-11-13 10:03:20 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 setup(
   name = 'superQuery',         
   packages = ['superQuery'],   
-  version = '2.3',     
+  version = '2.4',     
   license='MIT',       
   description = 'The Python interface to superQuery',  
   author = 'Eben du Toit',                 
   author_email = 'eben@superquery.io',     
   url = 'https://github.com/superquery/superPy',  
-  download_url = 'https://github.com/superquery/superPy/archive/v2.3.tar.gz',    
+  download_url = 'https://github.com/superquery/superPy/archive/v2.4.tar.gz',    
   keywords = ['DATA', 'SUPERQUERY', 'BIGQUERY'],
   install_requires=[            
       'pymysql',
       'pandas', 
-      'IPython'
+      'IPython',
+      'PyDrive'
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
